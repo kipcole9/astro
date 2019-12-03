a4778a5c5887ecc03c983b8a17e4c717a9500223
Kip Cole <kipcole9@gmail.com>
2019-12-04 07:22:20 +1100

    Refactor sunrise/sunset

--- lib/astro.ex
+++ lib/astro.ex
@@ -1,67 +1,14 @@
 defmodule Astro do
   alias Astro.{Time, Solar, Utils}
 
-  @utc_zone "Etc/UTC"
-
   def sunrise(location, date, options \\ default_options()) when is_list(options) do
     options = Keyword.put(options, :rise_or_set, :rise)
-    sun_rise_or_set(location, date, options)
+    Solar.sun_rise_or_set(location, date, options)
   end
 
   def sunset(location, date, options \\ default_options()) when is_list(options) do
     options = Keyword.put(options, :rise_or_set, :set)
-    sun_rise_or_set(location, date, options)
-  end
-
-  def sun_rise_or_set(location, date, options) when is_list(options) do
-    options =
-      default_options()
-      |> Keyword.merge(options)
-      |> Map.new
-
-    sun_rise_or_set(location, date, options)
-  end
-
-  def sun_rise_or_set(%Geo.PointZ{} = location, %Date{} = date, options) do
-    with {:ok, naive_datetime} <-
-           NaiveDateTime.new(date.year, date.month, date.day, 0, 0, 0, {0, 0}, date.calendar) do
-      sun_rise_or_set(location, naive_datetime, options)
-    end
-  end
-
-  def sun_rise_or_set(%Geo.PointZ{} = location, %NaiveDateTime{} = datetime, options) do
-    %{time_zone_database: time_zone_database} = options
-
-    with {:ok, iso_datetime} <- NaiveDateTime.convert(datetime, Calendar.ISO),
-         {:ok, time_zone} <- Time.timezone_at(location),
-         {:ok, utc_datetime} <- DateTime.from_naive(iso_datetime, time_zone, time_zone_database) do
-      sun_rise_or_set(location, utc_datetime, options)
-    end
-  end
-
-  def sun_rise_or_set(%Geo.PointZ{} = location, %DateTime{} = datetime, options) do
-    %{time_zone_database: time_zone_database} = options
-    with {:ok, iso_datetime} <- DateTime.convert(datetime, Calendar.ISO),
-         {:ok, utc_datetime} <- DateTime.shift_zone(iso_datetime, @utc_zone, time_zone_database),
-         moment_of_sunrise = utc_sun_rise_or_set(utc_datetime, location, options),
-         {:ok, utc_sunrise} <- Time.moment_to_datetime(moment_of_sunrise, utc_datetime),
-         {:ok, zone_sunrise} <-
-           Time.datetime_in_requested_zone(utc_sunrise, datetime.time_zone, location, options) do
-      DateTime.convert(zone_sunrise, datetime.calendar)
-    end
-  end
-
-  def sun_rise_or_set(location, datetime, options) do
-    Utils.normalize_location(location)
-    |> sun_rise_or_set(datetime, options)
-  end
-
-  def utc_sun_rise_or_set(utc_datetime, location, %{rise_or_set: :rise} = options) do
-    Solar.utc_sunrise(utc_datetime, location, options)
-  end
-
-  def utc_sun_rise_or_set(utc_datetime, location, %{rise_or_set: :set} = options) do
-    Solar.utc_sunset(utc_datetime, location, options)
+    Solar.sun_rise_or_set(location, date, options)
   end
 
   def default_options do
--- lib/astro/solar.ex
+++ lib/astro/solar.ex
@@ -5,7 +5,60 @@ defmodule Astro.Solar do
 
   """
 
-  import Astro.{Utils, Earth, Time}
+  alias Astro.{Utils, Earth, Time}
+
+  @utc_zone "Etc/UTC"
+
+  def sun_rise_or_set(location, date, options) when is_list(options) do
+    options =
+      Astro.default_options()
+      |> Keyword.merge(options)
+      |> Map.new
+
+    sun_rise_or_set(location, date, options)
+  end
+
+  def sun_rise_or_set(%Geo.PointZ{} = location, %Date{} = date, options) do
+    with {:ok, naive_datetime} <-
+           NaiveDateTime.new(date.year, date.month, date.day, 0, 0, 0, {0, 0}, date.calendar) do
+      sun_rise_or_set(location, naive_datetime, options)
+    end
+  end
+
+  def sun_rise_or_set(%Geo.PointZ{} = location, %NaiveDateTime{} = datetime, options) do
+    %{time_zone_database: time_zone_database} = options
+
+    with {:ok, iso_datetime} <- NaiveDateTime.convert(datetime, Calendar.ISO),
+         {:ok, time_zone} <- Time.timezone_at(location),
+         {:ok, utc_datetime} <- DateTime.from_naive(iso_datetime, time_zone, time_zone_database) do
+      sun_rise_or_set(location, utc_datetime, options)
+    end
+  end
+
+  def sun_rise_or_set(%Geo.PointZ{} = location, %DateTime{} = datetime, options) do
+    %{time_zone_database: time_zone_database} = options
+    with {:ok, iso_datetime} <- DateTime.convert(datetime, Calendar.ISO),
+         {:ok, utc_datetime} <- DateTime.shift_zone(iso_datetime, @utc_zone, time_zone_database),
+         moment_of_sunrise = utc_sun_rise_or_set(utc_datetime, location, options),
+         {:ok, utc_sunrise} <- Time.moment_to_datetime(moment_of_sunrise, utc_datetime),
+         {:ok, zone_sunrise} <-
+           Time.datetime_in_requested_zone(utc_sunrise, datetime.time_zone, location, options) do
+      DateTime.convert(zone_sunrise, datetime.calendar)
+    end
+  end
+
+  def sun_rise_or_set(location, datetime, options) do
+    Utils.normalize_location(location)
+    |> sun_rise_or_set(datetime, options)
+  end
+
+  def utc_sun_rise_or_set(utc_datetime, location, %{rise_or_set: :rise} = options) do
+    utc_sunrise(utc_datetime, location, options)
+  end
+
+  def utc_sun_rise_or_set(utc_datetime, location, %{rise_or_set: :set} = options) do
+    utc_sunset(utc_datetime, location, options)
+  end
 
   @solar_elevation %{
     geometric: 90.0,
@@ -42,24 +95,24 @@ defmodule Astro.Solar do
   end
 
   def utc_sun_position(date, %Geo.PointZ{coordinates: {lng, lat, alt}}, solar_elevation, mode) do
-    adjusted_solar_elevation = adjusted_solar_elevation(solar_elevation, alt)
+    adjusted_solar_elevation = Earth.adjusted_solar_elevation(solar_elevation, alt)
 
     utc_time_in_minutes =
-      calculate_utc_sun_position(ajd(date), lat, -lng, adjusted_solar_elevation, mode)
+      calculate_utc_sun_position(Time.ajd(date), lat, -lng, adjusted_solar_elevation, mode)
 
-    mod(utc_time_in_minutes / 60.0, 24.0)
+    Utils.mod(utc_time_in_minutes / 60.0, 24.0)
   end
 
   def calculate_utc_sun_position(julian_day, latitude, longitude, solar_elevation, mode) do
-    julian_centuries = julian_centuries_from_julian_day(julian_day)
+    julian_centuries = Time.julian_centuries_from_julian_day(julian_day)
 
     # first pass using solar noon
     noonmin = solar_noon_utc(julian_centuries, longitude)
-    tnoon = julian_centuries_from_julian_day(julian_day + noonmin / 1440.0)
+    tnoon = Time.julian_centuries_from_julian_day(julian_day + noonmin / 1440.0)
     first_pass = approximate_utc_sun_position(tnoon, latitude, longitude, solar_elevation, mode)
 
     # refine using output of first pass
-    trefinement = julian_centuries_from_julian_day(julian_day + first_pass / 1440.0)
+    trefinement = Time.julian_centuries_from_julian_day(julian_day + first_pass / 1440.0)
     approximate_utc_sun_position(trefinement, latitude, longitude, solar_elevation, mode)
   end
 
@@ -74,7 +127,7 @@ defmodule Astro.Solar do
     solar_dec = solar_declination(approx_julian_centuries)
     hour_angle = sun_hour_angle_at_horizon(latitude, solar_dec, solar_elevation, mode)
 
-    delta = longitude - to_degrees(hour_angle)
+    delta = longitude - Utils.to_degrees(hour_angle)
     time_delta = delta * 4.0
     720.0 + time_delta - eq_time
   end
@@ -83,9 +136,9 @@ defmodule Astro.Solar do
   Returns the hour angle in radians
   """
   def sun_hour_angle_at_horizon(latitude, solar_dec, solar_elevation, mode) do
-    lat_r = to_radians(latitude)
-    solar_dec_r = to_radians(solar_dec)
-    solar_elevation_r = to_radians(solar_elevation)
+    lat_r = Utils.to_radians(latitude)
+    solar_dec_r = Utils.to_radians(solar_dec)
+    solar_elevation_r = Utils.to_radians(solar_elevation)
 
     hour_angle =
       :math.acos(
@@ -104,13 +157,13 @@ defmodule Astro.Solar do
   Returns the solar declination in degrees
   """
   def solar_declination(julian_centuries) do
-    correction = obliquity_correction(julian_centuries) |> to_radians
-    lambda = sun_apparent_longitude(julian_centuries) |> to_radians
+    correction = obliquity_correction(julian_centuries) |> Utils.to_radians
+    lambda = sun_apparent_longitude(julian_centuries) |> Utils.to_radians
     sint = :math.sin(correction) * :math.sin(lambda)
 
     sint
     |> :math.asin()
-    |> to_degrees
+    |> Utils.to_degrees
   end
 
   @doc """
@@ -119,7 +172,7 @@ defmodule Astro.Solar do
   def sun_apparent_longitude(julian_centuries) do
     true_longitude = sun_true_longitude(julian_centuries)
     omega = 125.04 - 1934.136 * julian_centuries
-    true_longitude - 0.00569 - 0.00478 * :math.sin(to_radians(omega))
+    true_longitude - 0.00569 - 0.00478 * :math.sin(Utils.to_radians(omega))
   end
 
   @doc """
@@ -133,7 +186,7 @@ defmodule Astro.Solar do
   Return the suns equation of time in degrees
   """
   def sun_equation_of_center(julian_centuries) do
-    mrad = sun_geometric_mean_anomaly(julian_centuries) |> to_radians
+    mrad = sun_geometric_mean_anomaly(julian_centuries) |> Utils.to_radians
     sinm = :math.sin(mrad)
     sin2m = :math.sin(2 * mrad)
     sin3m = :math.sin(3 * mrad)
@@ -144,15 +197,15 @@ defmodule Astro.Solar do
   end
 
   def solar_noon_utc(julian_centuries, longitude) do
-    century_start = julian_day_from_julian_centuries(julian_centuries)
+    century_start = Time.julian_day_from_julian_centuries(julian_centuries)
 
     # first pass to yield approximate solar noon
-    approx_tnoon = julian_centuries_from_julian_day(century_start + longitude / 360.0)
+    approx_tnoon = Time.julian_centuries_from_julian_day(century_start + longitude / 360.0)
     approx_eq_time = equation_of_time(approx_tnoon)
     approx_sol_noon = 720.0 + longitude * 4.0 - approx_eq_time
 
     # refinement using output of first pass
-    tnoon = julian_centuries_from_julian_day(century_start - 0.5 + approx_sol_noon / 1440.0)
+    tnoon = Time.julian_centuries_from_julian_day(century_start - 0.5 + approx_sol_noon / 1440.0)
     eq_time = equation_of_time(tnoon)
     720.0 + longitude * 4.0 - eq_time
   end
@@ -161,9 +214,9 @@ defmodule Astro.Solar do
   Returns the euation of time in minutes
   """
   def equation_of_time(julian_centuries) do
-    epsilon = obliquity_correction(julian_centuries) |> to_radians
-    sgml = sun_geometric_mean_longitude(julian_centuries) |> to_radians
-    sgma = sun_geometric_mean_anomaly(julian_centuries) |> to_radians
+    epsilon = obliquity_correction(julian_centuries) |> Utils.to_radians
+    sgml = sun_geometric_mean_longitude(julian_centuries) |> Utils.to_radians
+    sgma = sun_geometric_mean_anomaly(julian_centuries) |> Utils.to_radians
     eoe = earth_orbit_eccentricity(julian_centuries)
 
     y = :math.tan(epsilon / 2.0)
@@ -179,7 +232,7 @@ defmodule Astro.Solar do
       y * sin2l0 - 2.0 * eoe * sinm + 4.0 * eoe * y * sinm * cos2l0 - 0.5 * y * y * sin4l0 -
         1.25 * eoe * eoe * sin2m
 
-    to_degrees(eq_time) * 4.0
+    Utils.to_degrees(eq_time) * 4.0
   end
 
   @doc """
@@ -194,7 +247,7 @@ defmodule Astro.Solar do
   """
   def sun_geometric_mean_anomaly(julian_centuries) do
     anomaly = 357.52911 + julian_centuries * (35999.05029 - 0.0001537 * julian_centuries)
-    mod(anomaly, 360.0)
+    Utils.mod(anomaly, 360.0)
   end
 
   @doc """
@@ -202,7 +255,7 @@ defmodule Astro.Solar do
   """
   def sun_geometric_mean_longitude(julian_centuries) do
     longitude = 280.46646 + julian_centuries * (36000.76983 + 0.0003032 * julian_centuries)
-    mod(longitude, 360.0)
+    Utils.mod(longitude, 360.0)
   end
 
   @doc """
@@ -212,8 +265,8 @@ defmodule Astro.Solar do
     obliquity_of_ecliptic = mean_obliquity_of_ecliptic(julian_centuries)
 
     omega = 125.04 - 1934.136 * julian_centuries
-    correction = obliquity_of_ecliptic + 0.00256 * :math.cos(to_radians(omega))
-    mod(correction, 360.0)
+    correction = obliquity_of_ecliptic + 0.00256 * :math.cos(Utils.to_radians(omega))
+    Utils.mod(correction, 360.0)
   end
 
   @doc """
